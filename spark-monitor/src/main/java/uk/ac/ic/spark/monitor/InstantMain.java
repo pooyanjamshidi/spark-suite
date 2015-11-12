94b1f358cc52a94f1f4ddec2b8caa2f95b5840bf
RCheungIT <hubert.zhang@outlook.com>
2015-11-12 11:54:01 +0000

    add validation for params

--- spark-monitor/src/main/java/uk/ac/ic/spark/monitor/InstantMain.java
+++ spark-monitor/src/main/java/uk/ac/ic/spark/monitor/InstantMain.java
@@ -1,9 +1,11 @@
 package uk.ac.ic.spark.monitor;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import org.apache.commons.cli.*;
+import org.apache.commons.lang3.Validate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,57 +25,91 @@ public class InstantMain {
         try {
             // parse the command line arguments
             log.info("Get args: " + Arrays.toString(args));
+
             CommandLine line = parser.parse( options, args);
 
+            log.info("params: " + line.getOptionValue("P"));
 
-            Multimap<String, String> paramsMultiMap = parseParams(line.getOptionValues("P"));
+            String paramsAndValues = line.getOptionValue("P");
 
-            log.info("params List: " + paramsMultiMap);
+            Validate.matchesPattern(paramsAndValues, "\\[(\\w*=(\\w*,)*\\w*;)*\\w*=(\\w*,)*\\w*\\]",
+                    paramsAndValues + " does not look like [p1=v11,v12;p2=v21,v22]");
 
+
+            Multimap<String, String> paramsMultiMap = parseParams(Splitter.on(";")
+                    .trimResults(CharMatcher.is('['))
+                    .trimResults(CharMatcher.is(']'))
+                    .split(paramsAndValues)
+                    .iterator());
+
+            log.info("parsed params List: " + paramsMultiMap);
             List<Map<String, String>> combinedParams = combineAllParams(paramsMultiMap);
 
-            log.info("combinedParams List: " + combinedParams);
+            log.info("combined params List: " + combinedParams);
+
+            log.info("jar file Path: " + line.getOptionValue("J"));
 
             for(Map<String, String> paramsMap : combinedParams){
                 //TODO:: change configfile and submit job.
             }
 
-        }
-        catch( ParseException exp ) {
+        } catch( ParseException exp ) {
             // oops, something went wrong
-            System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp( "sprak-suite", options );
+            String errorMessage = "Parsing failed. Reason: " + exp.getMessage();
+            System.err.println(errorMessage);
+            log.error(errorMessage );
+            printHelp();
+        } catch (IllegalArgumentException iae){
+            String errorMessage =  "Illegal argument param: " + iae.getMessage();
+            System.err.println(errorMessage);
+            log.error(errorMessage);
+            printHelp();
         }
+    }
+
+    private static void printHelp(){
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp( "sprak-suite", options );
     }
 
     private static void initOptions(){
 
-        Option opt = Option.builder("P").hasArgs()
+        Option paramsOpt = Option.builder("P").hasArgs()
                 .longOpt("params")
                 .required()
-                .argName("p1=v11,v12;p2=v21,v22")
+                .argName("[p1=v11,v12;p2=v21,v22]")
+                .hasArg()
                 .desc("params list")
-                .valueSeparator(';')
                 .build();
-        options.addOption(opt);
+
+
+        Option jarOpt = Option.builder("J").hasArgs()
+                .longOpt("jar")
+                .required()
+                .hasArg()
+                .desc("path of jar file")
+                .build();
+
+        options.addOption(paramsOpt);
+        options.addOption(jarOpt);
 
     }
 
-    private static Multimap<String, String>  parseParams(String[] paramNamesAndValues){
+
+
+
+    private static Multimap<String, String>  parseParams(Iterator<String> paramNamesAndValuesIterator){
+
         Multimap<String, String> paramsMultiMap = HashMultimap.create();
 
-        if(paramNamesAndValues.length >= 2){
-            paramsMultiMap.putAll(paramNamesAndValues[0],
-                Splitter.on(",").trimResults().split(paramNamesAndValues[1]));
+        if(paramNamesAndValuesIterator.hasNext()){
 
-            for(int i = 2; i < paramNamesAndValues.length; i++){
-
-                Iterator<String> paramNamesAndValue = Splitter.on("=").
-                        trimResults().split(paramNamesAndValues[i]).iterator();
-
-                paramsMultiMap.putAll(paramNamesAndValue.next(),
-                        Splitter.on(",").trimResults().split(paramNamesAndValue.next()));
+            while (paramNamesAndValuesIterator.hasNext()){
+                String paramNamesAndValue = paramNamesAndValuesIterator.next();
+                Iterator<String> paramNamesAndValueIterator = Splitter.on("=").
+                        trimResults().split(paramNamesAndValue).iterator();
+                paramsMultiMap.putAll(paramNamesAndValueIterator.next(),
+                        Splitter.on(",").trimResults().split(paramNamesAndValueIterator.next()));
             }
 
         } else {
@@ -87,7 +123,8 @@ public class InstantMain {
         List<Map<String, String>> combinedParams = new ArrayList<Map<String, String>>();
 
         for(String paramName : paramsMultiMap.keySet()){
-            combinedParams = combineAnotherParams(paramName, paramsMultiMap.get(paramName),
+            combinedParams = combineAnotherParams(paramName,
+                    paramsMultiMap.get(paramName),
                     combinedParams);
         }
 
